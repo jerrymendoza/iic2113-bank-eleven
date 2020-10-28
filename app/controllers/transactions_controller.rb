ecd8fbf2b0a7807234f432d9a949faba39a50014
Maria Luisa Claro <muclaro@uc.cl>
2020-10-27 22:38:45 -0300

    fix: deleted scaffold views

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,18 +1,10 @@
 class TransactionsController < ApplicationController
-  # before_action :set_transaction, only: [:show, :edit, :update, :destroy]
   before_action :set_new_transaction, only: %i[new_saving new_transfer]
   before_action :set_user_account, only: [:index]
 
   # GET /transactions
-  # GET /transactions.json
   def index
     @transactions = Transaction.all
-  end
-
-  # GET /transactions/1
-  # GET /transactions/1.json
-  def show
-    render json: @transaction
   end
 
   def new_transfer; end
@@ -24,12 +16,7 @@ class TransactionsController < ApplicationController
     @transaction = Transaction.new
   end
 
-  # GET /transactions/1/edit
-  # def edit
-  # end
-
   # POST /transactions
-  # POST /transactions.json
   def create
     transaction_type = params[:transaction_type].to_i
     amount = transaction_params[:amount].to_i
@@ -58,49 +45,9 @@ class TransactionsController < ApplicationController
       redirect_back(fallback_location: { action: "index",
                                          error: 'Error creating transaction.' })
     end
-
-    # respond_to do |format|
-    #   if @transaction.save
-    #     format.html { redirect_to @transaction, notice: 'Transaction was successfully created.' }
-    #     format.json { render :show, status: :created, location: @transaction }
-    #   else
-    #     format.html { render :new }
-    #     format.json { render json: @transaction.errors, status: :unprocessable_entity }
-    #   end
-    # end
   end
 
-  # PATCH/PUT /transactions/1
-  # PATCH/PUT /transactions/1.json
-  # def update
-  #   respond_to do |format|
-  #     if @transaction.update(transaction_params)
-  #       format.html { redirect_to @transaction, notice: 'Transaction was successfully updated.' }
-  #       format.json { render :show, status: :ok, location: @transaction }
-  #     else
-  #       format.html { render :edit }
-  #       format.json { render json: @transaction.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  # # DELETE /transactions/1
-  # # DELETE /transactions/1.json
-  # def destroy
-  #   @transaction.destroy
-  #   respond_to do |format|
-  #     format.html { redirect_to transactions_url,
-  #                   notice: 'Transaction was successfully destroyed.' }
-  #     format.json { head :no_content }
-  #   end
-  # end
-
   private
-
-  # Use callbacks to share common setup or constraints between actions.
-  # def set_transaction
-  #   @transaction = Transaction.find(params[:id])
-  # end
 
   def set_new_transaction
     @transaction = Transaction.new
