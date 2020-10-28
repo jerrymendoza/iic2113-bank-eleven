bddffa47666763204c241d17c26f41f34d4682c1
nfbalbontin <nfbalbontin@uc.cl>
2020-10-28 16:07:23 -0300

    root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
-  root to: "index#welcome"
+  root to: "/users/sign_in"
   get 'transactions/new_transfer', to: 'transactions#new_transfer'
   get 'transactions/new_saving', to: 'transactions#new_saving'
   get 'api/v1/transactions/date', to: 'api/v1/transactions#date'
