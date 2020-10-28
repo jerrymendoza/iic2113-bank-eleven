e2abe9ded565a9d1a94b9e509cbc1bfa68ef83d2
nfbalbontin <nfbalbontin@uc.cl>
2020-10-28 12:04:07 -0300

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
-  root to: "index#welcome"
-  get 'transactions/new_transfer', to: 'transactions#new_transfer'
-  get 'transactions/new_saving', to: 'transactions#new_saving'
-  get 'api/v1/transactions/date', to: 'api/v1/transactions#date'
+  root to: "index/welcome"
+  get 'transactions/new_transfer', to: 'transactions/new_transfer'
+  get 'transactions/new_saving', to: 'transactions/new_saving'
+  get 'api/v1/transactions/date', to: 'api/v1/transactions/date'
 
   scope path: '/api' do
     api_version(module: "Api::V1", path: { value: "v1" }, defaults: { format: 'json' }) do
