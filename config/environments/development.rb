db406189e3a5d7a6d6f71d5eeda056aeb0f4256e
nfbalbontin <nfbalbontin@uc.cl>
2020-10-29 19:55:49 -0300

    recnfigure port

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
-    port: 25,
+    port: 587,
     domain: "asciicasts.com",
     authentication: "plain",
     enable_starttls_auto: true,
