b5ad23845c98d76af3439dda2211fa81eaa7bdd1
nfbalbontin <nfbalbontin@uc.cl>
2020-10-29 19:51:17 -0300

    changing port

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,9 +30,10 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
-    port: 587,
+    port: 25,
     domain: "asciicasts.com",
     authentication: "plain",
+    enable_starttls_auto: true,
     # enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"]
