156010168a5dfe0bb8f0ddd61c140d3c0fbfa71b
nfbalbontin <nfbalbontin@uc.cl>
2020-10-29 19:30:54 -0300

    fixing root

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,5 +38,5 @@ Rails.application.configure do
     password: ENV["GMAIL_PASSWORD"]
   }
 
-  config.action_mailer.default_url_options = { host: "localhost:3000" }
+  config.action_mailer.default_url_options = { url: ENV["REDISTOGO_URL"] }
 end
