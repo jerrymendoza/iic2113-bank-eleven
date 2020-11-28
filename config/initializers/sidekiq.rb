0bc90ec2bd453c5895b97d854575159e9220e2e9
benjastraub <bstraub@uc.cl>
2020-11-28 11:26:57 -0300

    Added development config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,13 +1,17 @@
 Sidekiq.configure_server do |_config|
   ActiveRecord::Base.configurations[Rails.env.to_s]['pool'] = 30
+  _config.redis = { url: ENV.fetch('REDIS_URL_SIDEKIQ', 'redis://localhost:6379/1') }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV.fetch('REDIS_URL_SIDEKIQ', 'redis://localhost:6379/1') }
 end
 
 if Rails.env.production?
   Sidekiq.configure_server do |config|
     config.redis = { url: ENV["REDISTOGO_URL"] }
   end
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { url: ENV["REDISTOGO_URL"] }
+  Sidekiq.configure_client do |config|
+    config.redis = { url: ENV["REDISTOGO_URL"] }
+  end
 end
